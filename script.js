dc68bad8275e17915ab227b8577eb0b0a60d919f
Wyatt McMurry <wyattmcmurry1@gmail.com>
2020-09-01 19:48:10 -0400

    slightly changed css to eliminate the unwanted effects from overlapped borders and wrote a function that changes the calculator display to the value of the number clicked and saves that value as a number (not a string) to a variable for later calculation

--- script.js
+++ script.js
@@ -37,9 +37,22 @@ function operate(operator, x, y) {
     return result;
 }
 
+//adding a border to all the buttons
 let numberOperatorContainer = document.querySelector('#num-Operator-Container');
 let allButtons = numberOperatorContainer.children;
-
 for(let i = 0; i < allButtons.length; i++) {
-    allButtons[i].style.border = "1px solid black";
+    allButtons[i].style.border = "0.5px solid black";
+}
+
+//grab all numbers and give each one a function that changes the text content of
+//the calculator's display and stores that number value in chosenNumber for calculation
+
+let chosenNumber = 0;
+let allTheNumbers = document.getElementsByClassName('number');
+let resultDisplay = document.querySelector('#result-Display');
+for(let i = 0; i < allTheNumbers.length; i++) {
+    allTheNumbers[i].addEventListener('click', () => {
+        resultDisplay.textContent = allTheNumbers[i].textContent;
+        chosenNumber += parseInt(allTheNumbers[i].textContent);
+    })
 }
