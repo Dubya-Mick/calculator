5b7951ba86b16ee3c268b79f44bc3a12881202e7
Wyatt McMurry <wyattmcmurry1@gmail.com>
2020-09-07 17:01:18 -0400

    added functionality for the decimal button and added support for float calculations

--- script.js
+++ script.js
@@ -58,34 +58,80 @@ function allClear() {
     calcResult = '';
 }
 
+//function that calls the logs of the variables needed
+function callTheLogs() {
+    console.log(chosenOperator);
+    console.log(firstOperand);
+    console.log(chosenNumber); 
+    console.log(operatorCalc);
+    console.log(equalsCalc);
+    console.log(calcResult); 
+}
+
 let allClearButton = document.querySelector('#AC');
 allClearButton.addEventListener('click', () => {
     allClear();
     resultDisplay.textContent = '0';
+    callTheLogs();
     });
 
 //define function that calculates the input values
+//if the numbers are floats, they get parsed as floats
+//if they're integers they get parsed as integers
+//surrounding the function with () and adding the () 
+//at the end invokeas the function immediately
 function calculate() {
-    if(firstOperand != '' && chosenOperator != '' && chosenNumber != '') {
-        let numOperand = parseInt(firstOperand);
-        let numChosenNumber = parseInt(chosenNumber);
-        calcResult = operate(chosenOperator, numOperand, numChosenNumber);
-        resultDisplay.textContent = operate(chosenOperator, numOperand, numChosenNumber);
-        
-    }
+    let numOperand = (function() {
+        if (/[.]/.test(firstOperand)) {
+            return parseFloat(firstOperand); 
+        } else {
+            return parseInt(firstOperand);
+        }
+    }());
+    let numChosenNumber = (function() {
+        if (/[.]/.test(chosenNumber)) {
+            return parseFloat(chosenNumber); 
+        } else {
+            return parseInt(chosenNumber);
+        }
+    }());
+    calcResult = operate(chosenOperator, numOperand, numChosenNumber);
+    resultDisplay.textContent = calcResult;
 }
+
 //set the equals sign to execute the calculate function and change
 //the equalsCalc boolean
 let equalsSign = document.querySelector('#equals');
 equalsSign.addEventListener('click', () => {
-    calculate();
-    equalsCalc = true;
-    console.log(chosenNumber);
-    console.log(chosenOperator);
-    console.log(firstOperand);
-    console.log(equalsCalc);
-    console.log(operatorCalc);
+    if (firstOperand != '' && chosenOperator != '' && chosenNumber != '' && chosenNumber != '0.') {
+        calculate();
+        equalsCalc = true;
+        callTheLogs();
+    }
 });
+
+//define function of the decimal button
+let decimalButton = document.querySelector('#decimal');
+decimalButton.addEventListener('click', () => {
+    if(equalsCalc == true) {
+        allClear();
+        chosenNumber = "0.";
+        resultDisplay.textContent = "0.";
+    } else if (operatorCalc == true) {
+        operatorCalc = false;
+        firstOperand = calcResult;
+        chosenNumber = '0.';
+        resultDisplay.textContent = '0.'
+    } else if (resultDisplay.textContent == '0') {
+        resultDisplay.textContent = "0."
+        chosenNumber = "0."
+    } else if (!/[.]/.test(chosenNumber) && !/[.]/.test(resultDisplay.textContent)) {
+        chosenNumber += '.';
+        resultDisplay.textContent += '.';
+    }
+    callTheLogs();
+});
+
 
 
 //grab the operators and assign them values equivalent to their operation
@@ -104,21 +150,17 @@ for(let i = 0; i < allTheOperators.length; i++) {
         //if we have two numbers to calculate and an operator chosen
         //then the operator button calculates and feeds up the result
         //and updates the chosenOperator
-        } else if (chosenNumber != '' && firstOperand != '' && chosenOperator != '') {
+        } else if (chosenNumber != '' && firstOperand != '' && chosenOperator != '' && chosenNumber != '0.') {
             calculate();
             operatorCalc = true;
             chosenOperator = operatorClicked.textContent;
         //this else if is for the situation in which the firstOperand hasn't been chosen yet
-        } else if (chosenNumber != '') {
+        } else if (chosenNumber != '' && chosenNumber != '0.') {
             firstOperand = chosenNumber;
             chosenNumber = '';
             chosenOperator = operatorClicked.textContent;
         }
-        console.log(chosenNumber);
-        console.log(chosenOperator);
-        console.log(firstOperand);
-        console.log(equalsCalc);
-        console.log(operatorCalc);
+        callTheLogs();
     });
 }
 
@@ -134,6 +176,7 @@ for(let i = 0; i < allTheNumbers.length; i++) {
             operatorCalc = false;
             firstOperand = calcResult;
             chosenNumber = '';
+            resultDisplay.textContent = numberClicked.textContent;
         } else if (equalsCalc == true) {
             allClear();
             resultDisplay.textContent = numberClicked.textContent;
@@ -145,11 +188,7 @@ for(let i = 0; i < allTheNumbers.length; i++) {
            resultDisplay.textContent += numberClicked.textContent;
         }
         chosenNumber += numberClicked.textContent;
-        console.log(chosenNumber);
-        console.log(chosenOperator);
-        console.log(firstOperand);
-        console.log(equalsCalc);
-        console.log(operatorCalc);
+        callTheLogs();
    });
 }
 
